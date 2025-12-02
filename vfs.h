a4ad9c2632ad908145ecc8781cbe3fdf7cb79c99
itaminy <kirillgusss@gmail.com>
2025-12-02 17:51:54 +0300

    Update vfs.h

--- vfs.h
+++ vfs.h
@@ -1,4 +1,3 @@
-// vfs.h
 #ifndef VFS_H
 #define VFS_H
 
@@ -8,4 +7,4 @@ void fuse_start(void);
 // Вывод информации о разделе (используется командой \l /dev/...)
 void print_disk_info(const char *device);
 
-#endif // VFS_H
+#endif   
